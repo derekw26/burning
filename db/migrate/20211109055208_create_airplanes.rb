b930a089392b3733a6634a4cd27dbe68286f3a11
Goutham <goutham@Goutham-2.local>
2021-11-09 17:17:33 +1100

    db fix

--- db/migrate/20211109055208_create_airplanes.rb
+++ db/migrate/20211109055208_create_airplanes.rb
@@ -1,10 +1,9 @@
 class CreateAirplanes < ActiveRecord::Migration[5.2]
   def change
     create_table :airplanes do |t|
-      t.integer :row
-      t.integer :column
+      t.integer :rows
+      t.integer :columns
       t.text :name
-      t.integer :id
 
       t.timestamps
     end
